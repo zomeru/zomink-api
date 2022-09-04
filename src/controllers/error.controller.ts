e7cf5ae4194108817b3dc9f9f3deed143f97e7f4
Zomer <zomerzxc.19@gmail.com>
2022-09-04 17:44:29 +0800

    Fixed error handler

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -1,81 +1,25 @@
-// @ts-nocheck
+import { NextFunction, Response } from 'express';
 import { AppError } from '../utils/appError';
 
-const handleCastErrorDB = (err) => {
-  const message = `Invalid ${err.path}: ${err.value}.`;
-  return new AppError(message, 400);
-};
-
-const handleDuplicateFieldsDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
-  console.log(value);
-  const message = `Duplicate field value: ${value}. Please use another value.`;
-
-  return new AppError(message, 400);
-};
-
-const handleValidationErrorDB = (err) => {
-  const errors = Object.values(err.errors).map((el) => el.message);
-
-  const message = `Invalid input data. ${errors.join('. ')}`;
-  return new AppError(message, 400);
-};
-
-const handleJWTError = () =>
-  new AppError('Invalid token! Please login again.', 401);
-
-const handleJWTExpiredError = () =>
-  new AppError('Your token has expired! Please login again.', 401);
-
-const sendErrorDev = (err, res) => {
+const sendError = (err: AppError, res: Response) => {
   res.status(err.statusCode).json({
     status: 'error',
     statusCode: err.statusCode,
     message: err.message,
-    // stack: err.stack,
   });
 };
 
-const sendErrorProd = (err, res) => {
-  // Operational, trusted error
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-
-    // Programming or other unknown error: don't leak error details
-  } else {
-    // 1) Log error
-    console.error('ERROR', err);
-
-    // 2) Send generic message
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went wrong!',
-    });
-  }
-};
-
 // eslint-disable-next-line no-unused-vars
-export const globalErrorHandler = (err, req, res, next) => {
+export const globalErrorHandler = (
+  err: AppError,
+  _req: Request,
+  res: Response,
+  next: NextFunction // eslint-disable-line no-unused-vars
+) => {
   /* eslint-disable no-param-reassign */
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   /* eslint-enable no-param-reassign */
 
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
-
-    if (error.name === 'CastError') error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError')
-      error = handleValidationErrorDB(error);
-    if (error.name === 'JsonWebTokenError') error = handleJWTError();
-    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
-
-    sendErrorProd(error, res);
-  }
+  sendError(err, res);
 };
